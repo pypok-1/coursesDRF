6e3651986a511fd7607d91e4f24ff891368233d0
Eva <evacherkashyna9@gmail.com>
2026-02-04 16:29:02 +0200

    Update serializers.py

--- courses/serializers.py
+++ courses/serializers.py
@@ -16,12 +16,19 @@ class InstructorSerializer(serializers.ModelSerializer):
         model = Instructor
         fields = ('id', 'name', 'last_name', 'email')
 
+    def validate_experience(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Experience cannot be --neg")
+        return value
+
 
 class CourseSerializer(serializers.ModelSerializer):
     instructor = InstructorSerializer(read_only=True)
     students = StudentSerializer(many=True, read_only=True)
-    instructor_id = serializers.PrimaryKeyRelatedField(queryset=Instructor.objects.all(),source='instructor', write_only=True)
-    student_ids = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all(),source='students', write_only=True, many=True)
+    instructor_id = serializers.PrimaryKeyRelatedField(queryset=Instructor.objects.all(), source='instructor',
+                                                       write_only=True)
+    student_ids = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all(), source='students', write_only=True,
+                                                     many=True)
 
     class Meta:
         model = Course
@@ -36,8 +43,15 @@ class CourseSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         student_ids = validated_data.pop('student_ids', None)
         course = Course.objects.create(**validated_data)
-        course.students.set(student_ids)
+        if student_ids:
+            course.students.set(student_ids)
         return course
 
-
-
+    def update(self, instance, validated_data):
+        student_ids = validated_data.pop('student_ids', None)
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        if student_ids is not None:
+            instance.students.set(student_ids)
+        instance.save()
+        return instance
